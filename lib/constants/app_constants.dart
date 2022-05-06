f01e9bba68c17e389d19f9aa40026542c78530d8
shahanahmed <shahanahmeddev@gmail.com>
2022-05-06 23:48:25 +0500

    initial Push

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'http://3.17.189.59:8080/';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
